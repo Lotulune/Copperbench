01a78c23a6f790498f8ddf5ba80d548516ada18a
Lunafleur <54838716+Lotulune@users.noreply.github.com>
2026-09-01 23:47:26 +0800

    temporary reset marker

--- scripts/Invoke-G9CleanWindowsGuiGate.ps1
+++
@@ -1,9 +0,0 @@
-[CmdletBinding()]
-param(
-	[string]$VmName = 'Copperbench-G7',
-	[string]$RepositoryRoot = (Resolve-Path (Join-Path $PSScriptRoot '..')).Path,
-	[string]$GuestUser = 'g7admin',
-	[string]$PasswordFile = 'D:\Hyper-V\G7\g7admin.password.txt',
-	[string]$InstallDir = 'C:\Copperbench-G9',
-	[string]$WorkspaceName = 'guigatedelta'
-)
